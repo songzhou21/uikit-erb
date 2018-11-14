6307d1659eb458ee9241d8e595efa71199b85949
Song Zhou <zhousong1993@gmail.com>
2018-11-14 09:02:09 +0800

    separate view and view controller generation

--- ui_view_controller.rb
+++ ui_view_controller.rb
@@ -2,6 +2,42 @@ require 'erb'
 
 view_name = ARGV[0]
 
+view_interface = ERB.new <<-END
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface <%= view_name %>View : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
+END
+
+view_imp = ERB.new <<-END
+#import "<%= view_name %>View.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface <%= view_name %>View ()
+
+@end
+
+@implementation <%= view_name %>View 
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
+END
+
 interface = ERB.new <<-END
 #import <UIKit/UIKit.h>
 
@@ -17,24 +53,9 @@ END
 
 imp = ERB.new <<-END
 #import "<%= view_name %>ViewController.h"
+#import "<%= view_name %>View.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
-@interface <%= view_name %>View : UIView
-
-@end
-
-@implementation <%= view_name %>View 
-
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    if (self) {
-        
-    }
-    return self;
-}
-
-@end
 
 @interface <%= view_name %>ViewController ()
 
@@ -53,10 +74,6 @@ NS_ASSUME_NONNULL_BEGIN
     [super viewDidLoad];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
@@ -72,4 +89,12 @@ open("#{view_name}ViewController.h", "w") { |out|
 
 open("#{view_name}ViewController.m", "w") { |out|
     out.puts imp.result(binding)
+}
+
+open("#{view_name}View.h", "w") { |out|
+    out.puts view_interface.result(binding)
+}
+
+open("#{view_name}View.m", "w") { |out|
+    out.puts view_imp.result(binding)
 }
